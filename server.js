e6b8303b70c627ab0a1119f0a0174b0c7037a7db
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2025-09-22 16:56:55 +0100

    install packages (tailwind css and mysql , sequalize , express js , ...

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use("/css", express.static(path.join(__dirname, "public/css")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+
+module.exports = app;
